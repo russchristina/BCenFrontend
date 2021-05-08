a59659b4034c8530d3cc7208793c380b673398ff
russ.christinas <russ.christinas@gmail.com>
2021-05-08 16:13:50 -0500

    added deselection logic

--- src/app/components/volunteering/volunteering.component.ts
+++ src/app/components/volunteering/volunteering.component.ts
@@ -11,6 +11,7 @@ import { User } from 'src/app/models/User';
 
 //I need to add an array or JS object that tracks whether or not each check mark is "active" or "inactive".
 
+//Currently addressing unchecking a box and thus unregistering for a task.
 @Component({
   selector: 'app-volunteering',
   templateUrl: './volunteering.component.html',
@@ -26,12 +27,23 @@ export class VolunteeringComponent implements OnInit {
 
   responsibilities:Responsibility[] = [];
   userSelectedResponsibilities:Responsibility[] = []
+  checkBoxStates:boolean[] = []
+
+  initializeCheckboxStates():void{
+    for(let r of this.responsibilities){
+      //All checkbox states are false to begin with, meaning they aren't checked.
+      this.checkBoxStates.push(false)
+    }
+  }
 
   findAllResponsibilities():void{
     this.responsibilityService.findAll().subscribe(
       (data) => {
         this.responsibilities = data
         console.log(this.responsibilities)
+        //Checkbox states are initialized here because the responsibilities are populated asynchronously
+        this.initializeCheckboxStates()
+        console.log(this.checkBoxStates)
       },
       () => {
         console.log('Ooops! Something went wrong!')
@@ -93,6 +105,9 @@ export class VolunteeringComponent implements OnInit {
          console.log('Could not get cached user.')
        }
      )
+
+     //abstract out into isSignedUp
+     //Check that the user is not already signed up for a responsibility
      for(let r of this.responsibilities){
        if(r.id === responsibilityId){
          for(let u of r.users){
@@ -104,24 +119,53 @@ export class VolunteeringComponent implements OnInit {
        }
      }
 
-     console.log('it makes it past the already signed up check')
-
     //Find the responsibility in the list of available responsibilities by its ID and check the number of participants. I need to make sure that a user cannot sign up for a responsibility that is at its limit for participants.
+    //Yikes. I'm tired of iterating through this array, but it's also the case that if I ever just rely on the index alone, I have to make sure that I haven't changed the order of the elements of even removed an element from the array (well, pseudo removal via making an entirely new array) because that will completely throw off any algorithm which depends on a certain element have a certain index
     for(let r of this.responsibilities){
-      console.log('the limit')
-      console.log(r.id)
-      console.log(responsibilityId)
       if(r.id === responsibilityId && r.users.length < r.upperlimit){
+        if(this.checkBoxStates[r.id - 1] == false){
         this.userCacheService.getUserData().subscribe(
           (user) => {
             r.users.push(user)
             console.log('user added')
+            //Unforuntately, this depends on nothing about the order of the responsibilities in the array changing. This makes my stomach hurt.
+            this.checkBoxStates[r.id - 1] = true
+            console.log(this.checkBoxStates)
           },
           () => {
             console.log('Cannot access cached user.')
           }
-        )
-       //Also update the responsibility info in the DB. I currently do no not have a means of doing this on the backend though. You might want to do this in a lifecycle hook!
+        )}else{
+          //deselection occurs
+          this.deselectResponsibility(r.id)
+          this.checkBoxStates[r.id - 1] = false
+        }
+      }
+    }
+  }
+
+  deselectResponsibility(responsibilityId:number):void{
+
+    let currentUser = new User()
+    this.userCacheService.getUserData().subscribe(
+      (data) => {
+        currentUser = data
+      },
+      () => {
+        console.log('Could not fetch current user!')
+      }
+    )
+    //remove the user from the responsibility
+    for(let r of this.responsibilities){
+      if(r.id === responsibilityId){
+        //The user IDs are not in order, so I can't use an enhanced for loop to move through the users to splice by index later. Ouch.
+        for(let i = 0; i < r.users.length; i++){
+          //Based on your implementation, this should be true anyway.
+          if(r.users[i].name === currentUser.name){
+            //Remove this user from the users array for the responsibility
+            console.log(r.users.splice(i, 1))
+          }
+        }
       }
     }
   }
