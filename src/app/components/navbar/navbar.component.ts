a0426d8993ac2f329e6f59fc6ba6659f6af50189
russ.christinas <russ.christinas@gmail.com>
2021-05-08 17:03:04 -0500

    ngIf and route guarding

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UserCacheService } from 'src/app/services/user-cache.service';
+import { User } from 'src/app/models/User';
 
 @Component({
   selector: 'app-navbar',
@@ -10,10 +12,19 @@ export class NavbarComponent implements OnInit {
   name = "anchLinks";
   ids: Array<String> = ["About", "Rules"]
 
-  constructor() { }
+  constructor(private userCacheService:UserCacheService) { }
 
   ngOnInit(): void {
+    this.userCacheService.getUserData().subscribe(
+      (data) => {
+        this.currentUser = data
+      },
+      () => {
+        console.log('Error: Could not get current user data')
+      }
+    )
   }
 
+  currentUser:User = null
  
 }
