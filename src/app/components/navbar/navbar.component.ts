10c10cab59de1124806473dc9cf1650c02b2ff3b
Brianna Bullock <brianna.bullock16@gmail.com>
2021-05-08 16:25:30 -0500

    set navcollapse to true

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-
+  isNavbarCollapsed = true
   name = "anchLinks";
   ids: Array<String> = ["About", "Rules"]
 
